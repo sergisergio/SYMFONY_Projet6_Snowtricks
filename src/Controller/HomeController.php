685b21011342c7ba21ec5e2cf8970b8ddfc4e4aa
ptraon <ptraon@gmail.com>
2018-07-11 18:12:17 +0200

    add web debug toolbar

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -3,9 +3,10 @@
 namespace App\Controller;
 
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 
-class HomeController
+class HomeController extends AbstractController
 {
     /**
      * @Route("/")
