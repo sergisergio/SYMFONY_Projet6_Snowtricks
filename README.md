e5b4bcf5c31da7712bdcdfead1271fd9e091890c
ptraon <ptraon@gmail.com>
2018-07-12 18:59:26 +0200

    created 4 pages : homepage, trickpage, createtrickpage, modifytrickpage; also added a codacy badge

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # Snowtricks_Project6
+
+[![Codacy Badge](https://api.codacy.com/project/badge/Grade/8b9a914a39314f5eb479e3b47a176a56)](https://www.codacy.com/app/sergisergio/Snowtricks_Project6?utm_source=github.com&amp;utm_medium=referral&amp;utm_content=sergisergio/Snowtricks_Project6&amp;utm_campaign=Badge_Grade)
--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,17 +2,41 @@
 
 namespace App\Controller;
 
-use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 
 class HomeController extends AbstractController
 {
     /**
-     * @Route("/")
+     * @Route("/", name="app_homepage")
      */
     function homepage()
     {
-        return new Response('Hello !!!!!!');
+        return $this->render('homepage.html.twig');
+    }
+
+    /**
+     * @Route("/trick", name="app_trickpage")
+     */
+    function trickpage()
+    {
+        return $this->render('trick.html.twig');
+    }
+
+    /**
+     * @Route("/createtrick")
+     */
+    function createtrickpage()
+    {
+        return $this->render('createtrick.html.twig');
+    }
+
+    /**
+     * @Route("/modifytrick")
+     */
+    function modifytrickpage()
+    {
+        return $this->render('modifytrick.html.twig');
     }
 }
